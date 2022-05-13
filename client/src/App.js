377f8a5753b6142dfb2ffaa19051f4b0020fefc0
Ramy <rami.naccache1@gmail.com>
2022-05-13 23:38:01 +0200

    Updated Routes and removed Switch + Redirect for newer version

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import Home from "./pages/Home";
-import SingleProduct from "./pages/SingleProduct"
-import Pay from "./pages/Pay"
+import Pay from "./pages/Pay";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Success from "./pages/Success";
 
 const App = () => {
-  return <Pay/>;
+  
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/success" element={<Success/>} />
+        <Route path="*" element={<Navigate to="/"/>} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
