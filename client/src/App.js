479e4ae3a385d05f6f273761c8978712c0a631a6
Ramy <rami.naccache1@gmail.com>
2022-05-14 18:54:08 +0200

    Refreshed

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,10 @@ import {
   Navigate,
 } from "react-router-dom";
 import Success from "./pages/Success";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Register from "./pages/Register";
+
 
 const App = () => {
   
@@ -14,6 +18,9 @@ const App = () => {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home/>} />
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/Cart" element={<Cart/>} />
+        <Route path="/Register" element={<Register/>} />
         <Route path="/success" element={<Success/>} />
         <Route path="/Pay" element={<Pay/>} />
         <Route path="*" element={<Navigate to="/"/>} />
